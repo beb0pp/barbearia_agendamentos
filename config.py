6dc74762f38b3b165a3cd5d833a44d5eb5d49032
beb0pp <luss.fel@gmail.com>
2025-04-26 16:47:58 -0300

    Alterando permissionamentos e var de ambiente

--- config.py
+++ config.py
@@ -1,8 +1,13 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = 'sua_chave_secreta'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'chave-temporaria-desenvolvimento'
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(basedir, 'database', 'db.sqlite3')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    FLASK_ENV = os.environ.get('FLASK_ENV', 'production')
+    FLASK_DEBUG = os.environ.get('FLASK_DEBUG', '0') == '1'
